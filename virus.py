1bb57d056eff8a0772c7a7207f90a7e0593dfbc3
Bobdedob <K102@clayesmore.com>
2024-06-10 18:27:58 +0100

    Create virus.py

---
+++ virus.py
@@ -0,0 +1,7 @@
+import turtle
+t = turtle.Turtle()
+accesscode = input('...|')
+t.forward(9999999999999999999999999999999999999999999999999999999999999999999999)
+if accesscode == '123' :
+	t.clear()
+	t. hideturtle()
